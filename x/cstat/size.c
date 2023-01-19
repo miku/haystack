b108deb3855d50bc86edce8bdc06a42c9a24448f
Martin Czygan <martin.czygan@gmail.com>
2023-01-19 14:33:43 +0100

    use webkit style

--- x/cstat/size.c
+++ x/cstat/size.c
@@ -1,9 +1,11 @@
 // Size of the stat struct: 144 bytes.
+// https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html#index-struct-stat
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int main() {
-  struct stat buf;
-  printf("%ld\n", sizeof(buf));
+int main()
+{
+    struct stat buf;
+    printf("%ld\n", sizeof(buf));
 }
--- x/cstat/tabstat.c
+++ x/cstat/tabstat.c
@@ -28,47 +28,48 @@
 #include <sys/stat.h>
 #include <time.h>
 
-int main(int argc, char *argv[]) {
-  DIR *d;
-  struct dirent *dent;
-  d = opendir(".");
+int main(int argc, char* argv[])
+{
+    DIR* d;
+    struct dirent* dent;
+    d = opendir(".");
 
-  struct stat buf;
-  int status;
-  int i = 0;
+    struct stat buf;
+    int status;
+    int i = 0;
 
-  clock_t begin = clock();
-  if (d) {
-    while ((dent = readdir(d)) != NULL) {
-      status = stat(dent->d_name, &buf);
-      i++;
-      if (status != 0) {
-        return -1;
-      }
+    clock_t begin = clock();
+    if (d) {
+        while ((dent = readdir(d)) != NULL) {
+            status = stat(dent->d_name, &buf);
+            i++;
+            if (status != 0) {
+                return -1;
+            }
 
-      // Keep check low, but allow, e.g. tabstop -v.
-      if (argc == 2) {
-        int nlen = strlen(dent->d_name);
-        char short_name[16];
-        strncpy(short_name, dent->d_name, 15);
-        short_name[15] = '\0';
-        // st_blocks: this is the amount of disk space that the file occupies,
-        // measured in units of 512-byte blocks.
-        printf(
-            "%16s\t%8lu\t%8lu\t%u\t%lu\t%u\t%u\t%10lu\t%lu\t%lu\t%lu\t%lu\t%"
-            "lu\n",
-            short_name, buf.st_dev, buf.st_ino, buf.st_mode, buf.st_nlink,
-            buf.st_uid, buf.st_gid, buf.st_size, buf.st_atime, buf.st_mtime,
-            buf.st_ctime, buf.st_blocks, buf.st_blksize);
-      }
+            // Keep check low, but allow, e.g. tabstop -v.
+            if (argc == 2) {
+                int nlen = strlen(dent->d_name);
+                char short_name[16];
+                strncpy(short_name, dent->d_name, 15);
+                short_name[15] = '\0';
+                // st_blocks: this is the amount of disk space that the file occupies,
+                // measured in units of 512-byte blocks.
+                printf(
+                    "%16s\t%8lu\t%8lu\t%u\t%lu\t%u\t%u\t%10lu\t%lu\t%lu\t%lu\t%lu\t%"
+                    "lu\n",
+                    short_name, buf.st_dev, buf.st_ino, buf.st_mode, buf.st_nlink,
+                    buf.st_uid, buf.st_gid, buf.st_size, buf.st_atime, buf.st_mtime,
+                    buf.st_ctime, buf.st_blocks, buf.st_blksize);
+            }
+        }
+        closedir(d);
+    } else {
+        printf("failed to open dir\n");
+        return 1;
     }
-    closedir(d);
-  } else {
-    printf("failed to open dir\n");
-    return 1;
-  }
-  clock_t end = clock();
-  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-  fprintf(stderr, "%d stat calls in %f (%f per op)\n", i, time_spent,
-          time_spent / i);
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    fprintf(stderr, "%d stat calls in %f (%f per op)\n", i, time_spent,
+        time_spent / i);
 }
